bdf41700941f736f29213c5ea5a6247da051bd24
Nikkoola22 <nikkoola@gmail.com>
2025-09-18 20:30:28 +0200

    Version 0.0.3 - Force Vercel cache clear complet

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Force Vercel deployment - Version 0.0.1
-console.log('App Manus RH - Version 0.0.2 - Nettoyé et prêt pour production')
+// Force Vercel deployment - Version 0.0.3 - Cache clear
+console.log('App Manus RH - Version 0.0.3 - Cache clear complet')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
