970584e1cf70048440d0a16a7b43545703f5db6f
Nikkoola22 <nikkoola@gmail.com>
2025-09-18 20:02:37 +0200

    Force: Forcer le redéploiement Vercel avec modification main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Force Vercel deployment - Version 0.0.1
+console.log('App Manus RH - Version 0.0.1 - API Render Direct')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
