fba52cba3b16450c4c83920937ae16db98d9dfb3
Nikkoola22 <165650747+Nikkoola22@users.noreply.github.com>
2025-09-18 13:38:13 +0200

    Update label.jsx

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { cn } from '@/lib/lib/utils.jsx'
+import { cn } from '@/lib/utils.jsx'
 
 const Label = React.forwardRef(({ className, ...props }, ref) => (
   <label
